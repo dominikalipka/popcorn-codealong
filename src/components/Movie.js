523bffe17dc2de1c0bc3b7289de36cc2712af026
Dominika Lipka <dominika.lipka96@gmail.com>
2021-11-03 16:57:36 +0100

    Added popular badge, if the movie has high rating

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 function Movie(props) {
+
+  // let popularMovie;
+  // if (props.rating >= 9) {
+  //   popularMovie = <p> <strong>POPULAR</strong> </p>
+  // } 
+
     return (
-      <div className='movies'>
+      <div className="movies">
         <h3>{props.title}</h3>
         <img src={props.imgURL} alt="movie pic" />
         <p>Rating: {props.rating}</p>
+        {props.rating >= 9 && <p><strong>POPULAR</strong></p>}
+        {/* {popularMovie} */}
       </div>
     );
 }
