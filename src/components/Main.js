a9723279a9a7b83d7ff709eba2204a320fbf4525
Dominika Lipka <dominika.lipka96@gmail.com>
2021-11-03 14:52:49 +0100

    Displaying movies using .map()

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Main(props) {
-  return <div>main here</div>;
+  // components/ListDemo.js
+
+  const movies = [
+    { id: "AMLsriUkE", title: "Jurassic Park", director: "Steven Spielberg" },
+    { id: "6dKZxwwmN", title: "Sharknado", director: "Anthony C. Ferrante" },
+    { id: "z2uykusRE", title: "Titanic", director: "James Cameron" },
+  ];
+
+  return <div className='movies'>
+      <ul>
+          {movies.map((item) => { return (
+            <li key={item.id}>
+              {" "}
+              <h3>{item.title}</h3>
+              <p>Director: {item.director}</p>
+            </li>
+          )})}
+      </ul>
+  </div>;
 }
 
 export default Main;
